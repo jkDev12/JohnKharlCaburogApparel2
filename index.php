1a51868edb9f8c7853ac236a130719465353c30c
JohnKharl <jkgc2897@gmail.com>
2026-09-08 22:09:40 +0800

    deployed bg2

--- index.php
+++ index.php
@@ -103,19 +103,38 @@
         <div class="moto">
           <p class= "moto1">Quite Style<p>
           <p class= "moto2">Loud Impact<p>   
-          <p class= "motosub">Where product quality exceeds it’s price and exudes confidence<p>
+          <p class= "motosub">Where product quality exceeds its price and exudes confidence<p>
         </div>
 
 
         <div class="benefits">
-
+        <div class="diamonds">
           <img class="diamond1" src="images/Asset 4.png">
           <img class="diamond2" src="images/Asset 4.png">
           <img class="diamond3" src="images/Asset 4.png">
+        </div>
+        </div>
 
+
+      <div class=2nd section>
+
+        <div>
+          <img class="secondsectiontitlecard" src="images/2ndsectiontitlecard.png"
         </div>
 
       </div>
+
+</div>
+      <div class="bg2-space">
+
+        <div>
+          <img class="bg2" src="images/360_F_104948871_xsXOz7JdVHtfgYgbRcrTaORTRCcUiKmV.jpg">
+        </div>
+
+      </div>
+
+      </div>
+
     </body>
     
     
