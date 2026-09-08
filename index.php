555e3600956c85d64bb8e8af414b9debb9647dd6
JohnKharl <jkgc2897@gmail.com>
2026-09-09 02:41:49 +0800

    Galler done

--- index.php
+++ index.php
@@ -1,142 +1,115 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-
     <link rel="stylesheet" href="style.css">
 
     <title>John Kharl Caburog Apparel</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
-
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
-
 </head>
 
+<body>
 
-    <body>
+    <div class="page-wrapper">
 
-    
-      <div class="header">
+        <!-- HEADER -->
+        <div class="header">
 
-        <div class="header-left-section">
+            <div class="header-left-section">
 
-          <div class= "logo-space">
+                <div class="logo-space">
+                    <img class="logo" src="images/logo.png" alt="Logo">
+                </div>
 
-            <img class="logo" src="images/logo.png">
+                <div class="header-brand">
+                    <h2>John Kharl Caburog Apparel</h2>
+                </div>
 
-          </div>
+            </div>
 
+            <div class="header-right-section">
 
-          <div class= "header-brand">
-           
-            <h2> John Kharl Caburog Apparel
-          
+                <div class="top-buttons">
+                    <button class="menu-button">HOME</button>
+                </div>
+
+                <div class="top-buttons">
+                    <button class="menu-button">GALLERY</button>
+                </div>
+
+                <div class="top-buttons">
+                    <button class="menu-button">ABOUT</button>
+                </div>
+
+                <div class="top-buttons">
+                    <button class="menu-button">CONTACT</button>
+                </div>
+
+                <div class="top-buttons">
+                    <button class="login-button">LOG IN</button>
+                </div>
+
             </div>
 
         </div>
 
+        <!-- BG1 -->
+        <div class="bg1-space">
 
-        <div class="header-right-section">
+            <img class="bg1" src="images/depositphotos_312437386-stock-photo-black-monochromatic-clothes-accessories-beauty.jpg" alt="Model wearing black monochromatic clothes">
 
-          <div class="top-buttons">
-             
-            <div>
-              
-              <button class="menu-button">HOME</button>
-
+            <div class="moto">
+                <p class="moto1">Quite Style</p>
+                <p class="moto2">Loud Impact</p>
+                <p class="motosub">Where product quality exceeds its price and exudes confidence</p>
             </div>
 
-          </div>
+            <div class="benefits">
+                <div class="diamonds">
+                    <img class="diamond1" src="images/Asset 4.png" alt="">
+                    <img class="diamond2" src="images/Asset 4.png" alt="">
+                    <img class="diamond3" src="images/Asset 4.png" alt="">
+                </div>
+            </div>
 
-
-          <div class="top-buttons">
-
-            <div>
-            
-              <button class="menu-button">GALLERY</button>
-
+            <div class="second-section">
+                <img class="secondsectiontitlecard" src="images/2ndsectiontitlecard.png" alt="">
             </div>
 
         </div>
 
-        <div class="header-right-section">
-
-          <div class="top-buttons">
-             
-            <div>
-              
-              <button class="menu-button">ABOUT</button>
-
+        <!-- GALLERY -->
+        <div class="gallery-section">
+            <div id="clothes1" class="clothes">
+              <div class="product-title">Black Hoodie</div>
+              <img class="product-image" src="images/360_F_686721990_oWvTbLdyUo7snaBjhEjinVUdIqQ7Airr.jpg">
+              <button class="buy-button">Buy now</button>
             </div>
+            <div id="clothes2" class="clothes">
+              <div class="product-title">Grey T-Shirt</div>
+              <img class="product-image" src="images/360_F_868101027_BufTMf4kyfeVYfPnSLRiOvD2hnhbFYOO.jpg">
+              <button class="buy-button">Buy now</button>
+            </div>
+            <div id="clothes3" class="clothes">
+              <div class="product-title">Black Jeans</div>
+              <img class="product-image" src="images/30189775_59732655_600.webp">
+              <button class="buy-button">Buy now</button>
+            </div>
+            <div id="clothes4" class="clothes">
+              <div class="product-title">Grey Hoodie</div>
+              <img class="product-image" src="images/gray-hoodie-mockup-front-back-260nw-2697869589.jpg">
+              <button class="buy-button">Buy now</button>
+            </div>
+        </div>
 
-          </div>
+        <!-- BG2 -->
+        <div class="bg2-space">
+            <img class="bg2" src="images/360_F_104948871_xsXOz7JdVHtfgYgbRcrTaORTRCcUiKmV.jpg" alt="Background pattern">
+        </div>
 
-          
-<div class="top-buttons">
-    <div>
-        <button class="menu-button">CONTACT</button>
     </div>
-</div>
 
-<div class="top-buttons">
-    <div>
-        <button class="login-button">LOG IN</button>
-    </div>
-</div>
-
-    </div>
-    
-        </div>
-
-
-
-</div>
-
-      <div class="bg1-space">
-
-        <div>    
-          <img class="bg1" src="images/depositphotos_312437386-stock-photo-black-monochromatic-clothes-accessories-beauty.jpg">
-        </div> 
-        <div class="moto">
-          <p class= "moto1">Quite Style<p>
-          <p class= "moto2">Loud Impact<p>   
-          <p class= "motosub">Where product quality exceeds its price and exudes confidence<p>
-        </div>
-
-
-        <div class="benefits">
-        <div class="diamonds">
-          <img class="diamond1" src="images/Asset 4.png">
-          <img class="diamond2" src="images/Asset 4.png">
-          <img class="diamond3" src="images/Asset 4.png">
-        </div>
-        </div>
-
-
-      <div class=2nd section>
-
-        <div>
-          <img class="secondsectiontitlecard" src="images/2ndsectiontitlecard.png"
-        </div>
-
-      </div>
-
-</div>
-      <div class="bg2-space">
-
-        <div>
-          <img class="bg2" src="images/360_F_104948871_xsXOz7JdVHtfgYgbRcrTaORTRCcUiKmV.jpg">
-        </div>
-
-      </div>
-
-      </div>
-
-    </body>
-    
-    
+</body>
 </html>
-
